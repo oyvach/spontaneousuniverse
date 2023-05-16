91a0bc201b6b4482d7ae4413373460af97bc9c11
Øyvind <oy.christiansen@gmail.com>
2023-05-17 01:41:52 +0200

    build

---
+++ build/_app/immutable/nodes/2.973d4802.js
@@ -0,0 +1,42 @@
+import{S as Ce,i as ze,s as Be,k as o,q as r,a as c,l as i,m as s,r as h,h as a,c as d,n as _,J as Je,b as Ee,C as e,D as Ie,o as Ue}from"../chunks/index.b81a1c59.js";function Ye(R){let l,p,q,H,W,C,E,z,B,I,J,U,u,Se,Y,v,F,w,b,K,O,Q,X,M,g,S,Z,ee,t,k,te,ae,x,oe,ie,T,ne,le,L,se,re,N,he,ce,V,de,me,j,fe,pe,A,ve,ge,$,ue,we,D,be,ye,G,_e;return{c(){l=o("div"),p=o("h1"),q=r("Gravitation, Science and Data Visulisation"),H=c(),W=o("hr"),C=c(),E=o("p"),z=r(`This web page is currently under development, and will function as a
+    showcase for my research into the the dark sector physics in the
+    cosmological and astrophysical setting. Ideally it will provide interesting
+    reading and resources for both the general audience and other researchers
+    working on similar problems.`),B=c(),I=o("h2"),J=r("N-body simulations"),U=c(),u=o("img"),Y=c(),v=o("p"),F=r(`Shown above is a slice of the simulation box of a universe where the the
+    dark energy component is not the cosmological constant, but rather the
+    `),w=o("span"),b=o("a"),K=r("symmetron"),O=r(`
+    field, a dynamic component that can cluster. The field has the has the potential
+    `),Q=r(R[0]),X=r(`
+    in which the trace of the matter stress-energy tensor INSERT makes the extremum
+    at origin stable as long as INSERT, effectively screening the the effect of the
+    dynamical scalar field and making it behave like a cosmological constant for
+    high density locations. This will allow it to agree with solar system experiments,
+    while introducing novel phenomenology at cosmological scales, motivated by some
+    observational tensions. Whether it truly will be able to do so in consistency
+    with available and upcoming empirical data is something I will set out to explore
+    in my research.`),M=c(),g=o("div"),S=o("h2"),Z=r("NB: To do, web page:"),ee=c(),t=o("ul"),k=o("li"),te=r("Save header object so I do not have to rewrite code for each html file?"),ae=c(),x=o("li"),oe=r(`When switching web page, the entire page flashes... Possible to have
+      without flash?`),ie=c(),T=o("li"),ne=r(`Make sure that page behaves sensible when loading/different window sizes,
+      different platforms (cell phone) etc`),le=c(),L=o("li"),se=r("Server... github DNS error when changing domain for some reason..."),re=c(),N=o("li"),he=r("Add content..."),ce=c(),V=o("li"),de=r("Remove scrollbar?"),me=c(),j=o("li"),fe=r(`new object navigator on left shoulder with hyperlinks to different
+      sections`),pe=c(),A=o("li"),ve=r("Good method for citation/references?"),ge=c(),$=o("li"),ue=r("Animations..."),we=c(),D=o("li"),be=r("Videos (Youtube inline, local files, etc)"),ye=c(),G=o("li"),_e=r("Switch over to (vector graphic -> SVG, pics -> webp, movies -> webm)"),this.h()},l(f){l=i(f,"DIV",{class:!0});var m=s(l);p=i(m,"H1",{});var ke=s(p);q=h(ke,"Gravitation, Science and Data Visulisation"),ke.forEach(a),H=d(m),W=i(m,"HR",{}),C=d(m),E=i(m,"P",{});var xe=s(E);z=h(xe,`This web page is currently under development, and will function as a
+    showcase for my research into the the dark sector physics in the
+    cosmological and astrophysical setting. Ideally it will provide interesting
+    reading and resources for both the general audience and other researchers
+    working on similar problems.`),xe.forEach(a),B=d(m),I=i(m,"H2",{});var Te=s(I);J=h(Te,"N-body simulations"),Te.forEach(a),U=d(m),u=i(m,"IMG",{class:!0,src:!0}),Y=d(m),v=i(m,"P",{});var y=s(v);F=h(y,`Shown above is a slice of the simulation box of a universe where the the
+    dark energy component is not the cosmological constant, but rather the
+    `),w=i(y,"SPAN",{class:!0});var Le=s(w);b=i(Le,"A",{href:!0});var Ne=s(b);K=h(Ne,"symmetron"),Ne.forEach(a),Le.forEach(a),O=h(y,`
+    field, a dynamic component that can cluster. The field has the has the potential
+    `),Q=h(y,R[0]),X=h(y,`
+    in which the trace of the matter stress-energy tensor INSERT makes the extremum
+    at origin stable as long as INSERT, effectively screening the the effect of the
+    dynamical scalar field and making it behave like a cosmological constant for
+    high density locations. This will allow it to agree with solar system experiments,
+    while introducing novel phenomenology at cosmological scales, motivated by some
+    observational tensions. Whether it truly will be able to do so in consistency
+    with available and upcoming empirical data is something I will set out to explore
+    in my research.`),y.forEach(a),m.forEach(a),M=d(f),g=i(f,"DIV",{});var P=s(g);S=i(P,"H2",{});var Ve=s(S);Z=h(Ve,"NB: To do, web page:"),Ve.forEach(a),ee=d(P),t=i(P,"UL",{class:!0});var n=s(t);k=i(n,"LI",{});var je=s(k);te=h(je,"Save header object so I do not have to rewrite code for each html file?"),je.forEach(a),ae=d(n),x=i(n,"LI",{});var Ae=s(x);oe=h(Ae,`When switching web page, the entire page flashes... Possible to have
+      without flash?`),Ae.forEach(a),ie=d(n),T=i(n,"LI",{});var $e=s(T);ne=h($e,`Make sure that page behaves sensible when loading/different window sizes,
+      different platforms (cell phone) etc`),$e.forEach(a),le=d(n),L=i(n,"LI",{});var De=s(L);se=h(De,"Server... github DNS error when changing domain for some reason..."),De.forEach(a),re=d(n),N=i(n,"LI",{});var Ge=s(N);he=h(Ge,"Add content..."),Ge.forEach(a),ce=d(n),V=i(n,"LI",{});var Re=s(V);de=h(Re,"Remove scrollbar?"),Re.forEach(a),me=d(n),j=i(n,"LI",{});var Me=s(j);fe=h(Me,`new object navigator on left shoulder with hyperlinks to different
+      sections`),Me.forEach(a),pe=d(n),A=i(n,"LI",{});var Pe=s(A);ve=h(Pe,"Good method for citation/references?"),Pe.forEach(a),ge=d(n),$=i(n,"LI",{});var qe=s($);ue=h(qe,"Animations..."),qe.forEach(a),we=d(n),D=i(n,"LI",{});var He=s(D);be=h(He,"Videos (Youtube inline, local files, etc)"),He.forEach(a),ye=d(n),G=i(n,"LI",{});var We=s(G);_e=h(We,"Switch over to (vector graphic -> SVG, pics -> webp, movies -> webm)"),We.forEach(a),n.forEach(a),P.forEach(a),this.h()},h(){_(u,"class","fit-picture"),Je(u.src,Se="illustrasjon.jpeg")||_(u,"src",Se),_(b,"href","symmetron"),_(w,"class","textlink"),_(l,"class","content"),_(t,"class","myUL")},m(f,m){Ee(f,l,m),e(l,p),e(p,q),e(l,H),e(l,W),e(l,C),e(l,E),e(E,z),e(l,B),e(l,I),e(I,J),e(l,U),e(l,u),e(l,Y),e(l,v),e(v,F),e(v,w),e(w,b),e(b,K),e(v,O),e(v,Q),e(v,X),Ee(f,M,m),Ee(f,g,m),e(g,S),e(S,Z),e(g,ee),e(g,t),e(t,k),e(k,te),e(t,ae),e(t,x),e(x,oe),e(t,ie),e(t,T),e(T,ne),e(t,le),e(t,L),e(L,se),e(t,re),e(t,N),e(N,he),e(t,ce),e(t,V),e(V,de),e(t,me),e(t,j),e(j,fe),e(t,pe),e(t,A),e(A,ve),e(t,ge),e(t,$),e($,ue),e(t,we),e(t,D),e(D,be),e(t,ye),e(t,G),e(G,_e)},p:Ie,i:Ie,o:Ie,d(f){f&&a(l),f&&a(M),f&&a(g)}}}function Fe(R){let l=`$$
+      V_{\\text{eff}}  \\equiv V - \\ln A(\\phi) T_m = V_0 - \\frac{1}{2}\\mu^2\\phi^2
+      - \\frac{1}{3}\\kappa \\phi^3 + \\frac{1}{4}\\lambda \\phi^4 - \\ln A(\\phi) T_m,
+      $$`;return Ue(()=>{let p=document.createElement("script");p.src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js",document.head.append(p),p.onload=()=>{MathJax={tex:{inlineMath:[["$","$"],["\\(","\\)"]]},svg:{fontCache:"global"}}}}),[l]}class Oe extends Ce{constructor(l){super(),ze(this,l,Fe,Ye,Be,{})}}export{Oe as component};
