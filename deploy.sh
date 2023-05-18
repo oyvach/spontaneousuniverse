3a018d457e41c4c4408c4ee3a6755cfdc73c3033
Øyvind <oy.christiansen@gmail.com>
2023-05-18 14:16:58 +0200

    sync

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./gitsync.sh "sync" "master"
+../gitsync.sh "sync" "master"
 npm run build
 npx gh-pages -d build
-./gitsync.sh "sync" "master"
+../gitsync.sh "sync" "master"
 git checkout gh-pages
 touch .nojekyll
-./gitsync "added .nojekyll" "gh-pages"
+../gitsync "added .nojekyll" "gh-pages"
 git checkout master
